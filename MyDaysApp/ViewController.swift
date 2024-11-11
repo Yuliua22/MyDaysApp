0483a999c834f3c13e3b013b5e4695f0020d79e3
Yulia Prokopeva <uliaprokopeva2@gmail.com>
2024-11-11 17:20:31 +0300

    add backend

--- MyDaysApp/ViewController.swift
+++ MyDaysApp/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var infoLabel: UILabel!
+    @IBOutlet weak var resultButton: UIButton!
+    
+    private var numberOfDays = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        resultButton.layer.cornerRadius = 12
     }
 
-
+    @IBAction func datePicker(_ sender: UIDatePicker) {
+        let range = sender.date..<Date.now
+        numberOfDays = range.formatted(.components(style: .wide, fields: [.day]))
+    }
+    
+    @IBAction func resultButtonTapped() {
+        infoLabel.text = "Ты наслаждаешься жизнью уже \(numberOfDays)"
+    }
 }
 
